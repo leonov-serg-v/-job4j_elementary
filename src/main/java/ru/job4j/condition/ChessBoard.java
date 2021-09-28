ace0f786a1078bd2457587d6c725ba020de33e32
leonov-serg-v <leonov.serg.v@gmail.com>
2021-09-28 20:20:03 +0300

    4.3.6. Math.abs [#373616 #209337] (перепоставка-2)

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -5,13 +5,12 @@ public class ChessBoard {
 
         int rsl = 0;
 
-        if (!((x1 < 0) || (x2 < 0) || (y1 < 0) || (y2 < 0))) {
-           rsl = Math.abs(x1 - x2);
-           if (rsl != Math.abs(y1 - y2)) {
+        if ((x1 >= 0) && (x1 <= 7) && (x2 >= 0) && (x2 <= 7) && (y1 >= 0) && (y1 <= 7) && (y2 >= 0) && (y2 <= 7))  {
+            rsl = Math.abs(x1 - x2);
+            if (rsl != Math.abs(y1 - y2)) {
                 rsl = 0;
             }
         }
-
         return rsl;
     }
 }
