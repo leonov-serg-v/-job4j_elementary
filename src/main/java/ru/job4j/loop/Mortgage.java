7b154398001c4bbd25346a7a34938bc7c54ba01e
leonov-serg-v <leonov.serg.v@gmail.com>
2021-10-01 19:46:21 +0300

    5.8. Именование классов и переменных [#4087]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -2,6 +2,7 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
+
         int year = 1;
         double p = percent / 100;
         double debt = amount + amount * p;
