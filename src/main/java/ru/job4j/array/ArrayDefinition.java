403761c22c3d48bbc898d28c0e20f269871bcca0
leonov-serg-v <leonov.serg.v@gmail.com>
2021-10-04 22:05:56 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,20 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
 
         System.out.println("Размер массива ages : " + ages.length);
         System.out.println("Размер массива surnames : " + surnames.length);
         System.out.println("Размер массива : " + prices .length);
+
+        names[0] = "Петрович";
+        names[1] = "Семеныч";
+        names[2] = "Федорович";
+        names[3] = "Валентинович";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
