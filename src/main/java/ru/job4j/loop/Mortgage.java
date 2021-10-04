436f1bfbb95a1708528e8a24e11a2bb420a9e637
leonov-serg-v <leonov.serg.v@gmail.com>
2021-10-04 21:33:47 +0300

    5.7. Ипотека [#156315 #211082](перепоставка-3)

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -2,10 +2,10 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
-        int year = 1;
+        int year = 0;
         double p = percent / 100;
-        amount += amount * p;
-        while (amount > salary) {
+        //amount += amount * p;
+        while (amount > 0) {
             year++;
             amount += amount * p - salary;
         }
